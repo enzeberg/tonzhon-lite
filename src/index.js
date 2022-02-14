7ed306bb7b81761c4c0be3ecffe35259fc3c18ef
Li Enze <enzeberg@outlook.com>
2022-02-14 16:39:19 +0800

    delete registerServiceWorker.js

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 
+import reportWebVitals from './reportWebVitals';
+
 import App from './App';
 import store from './redux/store/';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
   <BrowserRouter>
@@ -19,4 +20,7 @@ if (module.hot) {
   module.hot.accept();
 }
 
-registerServiceWorker();
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
